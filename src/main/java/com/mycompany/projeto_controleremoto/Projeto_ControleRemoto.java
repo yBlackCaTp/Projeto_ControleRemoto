271d0f528fa3fa19f2428abf2e66f08e2c14be67
yBlackCaTp <paulo.murilo.dev@gmail.com>
2023-01-30 12:19:35 -0300

    first commit

---
+++ src/main/java/com/mycompany/projeto_controleremoto/Projeto_ControleRemoto.java
@@ -0,0 +1,14 @@
+package com.mycompany.projeto_controleremoto;
+public class Projeto_ControleRemoto {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        
+        c.abrirMenu();
+        
+        
+                
+    }
+}
